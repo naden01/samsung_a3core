22ef8bcc78a43c3eda2b2c197be8429d5d64633c
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-10-21 04:04:05 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,7 +14,3 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
