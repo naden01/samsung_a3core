58718adaa2c74cbf9f26da6650d7952930e04f3c
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-10-21 04:04:40 -0700

    import

--- twrp_a3core.mk
+++ twrp_a3core.mk
@@ -6,7 +6,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
@@ -17,10 +17,15 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from a3core device
 $(call inherit-product, device/samsung/a3core/device.mk)
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a3core/recovery/root,recovery/root)
+
 PRODUCT_DEVICE := a3core
 PRODUCT_NAME := twrp_a3core
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A032F
 PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
