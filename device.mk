6bf1c0375583f0d0265594b2bc592e543bcd10c4
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-10-20 23:46:02 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,20 @@
 #
 
 LOCAL_PATH := device/samsung/a3core
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
